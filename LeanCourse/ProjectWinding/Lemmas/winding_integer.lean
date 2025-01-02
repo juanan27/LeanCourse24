3441aaec79bfd7fabcb59629acef3753dfada984
Jorge Carrasco Coquillat <jorcarcoqui@gmail.com>
2025-01-02 17:09:45 +0100

    advances winding on circle

--- LeanCourse/ProjectWinding/Lemmas/winding_integer.lean
+++ LeanCourse/ProjectWinding/Lemmas/winding_integer.lean
@@ -20,6 +20,7 @@ import Mathlib.Analysis.Calculus.Deriv.Pow
 import Mathlib.Analysis.SpecialFunctions.Integrals
 import Mathlib.Analysis.Convolution
 import Mathlib.Data.Real.Irrational
+import Mathlib.Analysis.Complex.CauchyIntegral
 import Mathlib.MeasureTheory.Function.Jacobian
 import Mathlib.Algebra.GroupWithZero.Basic
 import LeanCourse.ProjectWinding.Definitions.curves
@@ -27,7 +28,8 @@ import LeanCourse.ProjectWinding.Definitions.curves
 open DifferentiableOn Finset
 open BigOperators Function Set Real Topology Filter
 open MeasureTheory Interval Convolution ENNReal
-
+--open scoped Real NNReal Interval Pointwise Topology
+open Complex MeasureTheory TopologicalSpace Metric Function Set Filter Asymptotics
 open Set unitInterval Finset Metric
 
 noncomputable section
@@ -95,6 +97,56 @@ apply h.div
 exact h'
 exact fun x a ↦ h_v x a
 }
+
+theorem division_continuous_ball (f : ℂ → ℂ ) (g : ℂ → ℂ ) (h : ContinuousOn f (closedBall 0 1))
+(h' : ContinuousOn g (closedBall 0 1)) (h_v : ∀ s ∈ closedBall 0 1, g s ≠ 0) : ContinuousOn (fun s ↦ f s / g s) (closedBall 0 1) := by {
+  apply h.div
+  exact h'
+  exact fun x a ↦ h_v x a}
+
+theorem inverse_continuous_ball (g : ℂ → ℂ)
+(h : ContinuousOn g (closedBall 0 1)) (h_v : ∀ s ∈ closedBall 0 1, g s ≠ 0) : ContinuousOn (fun s ↦ 1 / g s) (closedBall 0 1) := by {
+  let f : ℂ → ℂ := fun z ↦ 1
+  have hf : ContinuousOn f (closedBall 0 1) := by exact continuousOn_const
+  have hquot : ContinuousOn (fun s ↦ f s / g s) (closedBall 0 1) := by exact division_continuous_ball f g hf h h_v
+  exact hquot
+}
+
+-- We write the same theorems in the differentiable version
+
+theorem division_differentiable (f : ℂ → ℂ ) (g : ℂ → ℂ ) (hf : Differentiable ℂ f) (hg : Differentiable ℂ g ) (h₀ : ∀ z, g z ≠ 0):
+ Differentiable ℂ (fun s ↦ f s / g s) := by {
+  apply hf.div
+  trivial
+  tauto
+ }
+
+theorem inverse_differentiable (g : ℂ → ℂ)
+(h : Differentiable ℂ g ) (h_v : ∀ s, g s ≠ 0) : Differentiable ℂ (fun s ↦ 1 / g s)  := by {
+let f : ℂ → ℂ := fun z ↦ 1
+have hf : Differentiable ℂ f := by exact differentiable_const 1
+have hqout : Differentiable ℂ (fun s ↦ 1 / g s) := by exact division_differentiable (fun s ↦ 1) g hf h h_v
+exact hqout
+}
+
+theorem division_differentiable_ball (f : ℂ → ℂ ) (g : ℂ → ℂ ) (hf : ∀ z_1 ∈ closedBall 0 1, DifferentiableAt ℂ f z_1) (hg : ∀ z_1 ∈ closedBall 0 1, DifferentiableAt ℂ g z_1 ) (h₀ : ∀ z, g z ≠ 0):
+ ∀ z_1 ∈ closedBall 0 1, DifferentiableAt ℂ (fun s ↦ f s / g s) z_1 := by {
+  intro z_1 h_z1
+  specialize hf z_1 h_z1
+  specialize hg z_1 h_z1
+  apply hf.div
+  · exact hg
+  · tauto
+ }
+
+theorem inverse_differentiable_ball (g : ℂ → ℂ)
+(h : ∀ z_1 ∈ closedBall 0 1, DifferentiableAt ℂ g z_1) (h_v : ∀ s ∈ closedBall 0 1, g s ≠ 0) : ∀ s ∈ closedBall 0 1, DifferentiableAt ℂ (fun s ↦ 1 / g s) s  := by {
+  let f : ℂ → ℂ := fun z ↦ 1
+  intro s hs
+  have hf : ∀ s ∈ closedBall 0 1, DifferentiableAt  ℂ f s := by exact fun s a ↦ differentiableAt_const 1
+  have hquot : ∀ s ∈ closedBall 0 1, DifferentiableAt ℂ  (fun s ↦ f s / g s) s := by exact fun s a ↦ DifferentiableAt.div (hf s a) (h s a) (h_v s a)
+  exact hquot s hs
+  }
 
 lemma ftc (f : ℝ → ℂ) (hf : Continuous f) (a b : ℝ) :
     deriv (fun u ↦ ∫ x : ℝ in a..u, f x) b = f b :=
@@ -249,6 +301,158 @@ theorem ω_integer (γ : closed_curve) (z : ℂ) (h : ∀ t ∈ I , γ t ≠ z)
   simp[h_equal]
   exact hsuff
 }
+
+-- We evaluate the values of ω when γ is the unit circle:
+
+-- First we show the following useful equality:
+
+lemma contour_integral_eq_curve_integral (γ : closed_curve) (h_circle : ∀ t ∈ I, γ t = Complex.exp (Complex.I * 2*π* t)) (z : ℂ ) (g : ℂ → ℂ := fun z_1 ↦ 1 / (z_1 - z)):
+∫ (t : ℝ) in I, deriv γ t / (γ t - z) = ∮ (z_1 : ℂ) in C(0, 1), g z_1 := by {
+        rw[circleIntegral_def_Icc]
+        unfold circleMap
+        simp
+        have hsubs : ∫ (t : ℝ) in I, deriv γ.toFun t / (γ.toFun t - z) = ∫ (t : ℝ) in I, deriv γ.toFun t / (cexp («I» * 2 * ↑π * ↑t) - z) := by {
+          refine setIntegral_congr_ae₀ ?hs ?h -- should be easy to show
+          · exact nullMeasurableSet_Icc
+          · have haux : ∀ x ∈ I, deriv γ.toFun x / (γ.toFun x - z) = deriv γ.toFun x / (cexp («I» * 2 * ↑π * ↑x) - z) := by exact fun x a ↦ congrArg (HDiv.hDiv (deriv γ.toFun x)) (congrFun (congrArg HSub.hSub (h_circle x a)) z)
+            exact ae_of_all volume haux
+        }
+        rw[hsubs]
+        have hsubs_2 : ∫ (t : ℝ) in I, deriv γ.toFun t / (cexp («I» * 2 * ↑π * ↑t) - z) = ∫ (t : ℝ) in I, deriv cexp («I» * 2 * ↑π * ↑t) / (cexp («I» * 2 * ↑π * ↑t) - z) := by {
+          refine setIntegral_congr_ae₀ ?hs ?_
+          have haux : ∀ x ∈ I, deriv γ.toFun x = deriv cexp («I» * 2 * ↑π * ↑x) := by {
+            intro x hx
+            specialize h_circle x hx
+            refine ftc_2 (fun x ↦ deriv cexp («I» * 2 * ↑π * ↑x)) ?hf γ.toFun x hx
+            refine Continuous.comp_continuousOn' ?hf.hg ?hf.hf
+            have hexp : deriv cexp = cexp := by exact Complex.deriv_exp
+            rw[hexp]
+            exact Complex.continuous_exp
+            fun_prop
+            }
+          have haux_final : ∀ x ∈ I, deriv γ.toFun x / (cexp («I» * 2 * ↑π * ↑x) - z) = deriv cexp («I» * 2 * ↑π * ↑x) / (cexp («I» * 2 * ↑π * ↑x) - z) := by exact fun x a ↦ congrFun (congrArg HDiv.hDiv (haux x a)) (cexp («I» * 2 * ↑π * ↑x) - z)
+          exact ae_of_all volume haux_final
+          }
+        rw[hsubs_2]
+        have hθ : ∀ θ ∈ Set.Icc 0 (2 * π), deriv (fun θ ↦ cexp (↑θ * «I»)) θ = Complex.I * cexp (↑θ * «I») := by {
+          intro θ hθ
+          let f : ℝ → ℂ := (fun θ ↦ (↑θ * «I»))
+          have hdiff : DifferentiableAt ℝ f θ := by {
+            simp_rw[f]
+            apply DifferentiableAt.mul_const
+            sorry
+          }
+          rw[deriv_cexp hdiff (f := f)]
+          have hderiv : deriv f θ = Complex.I := by {
+            simp_rw[f]
+            sorry
+
+          }
+          rw[hderiv]
+          simp_rw[f]
+          ring
+        }
+        have haux : ∀ θ ∈ Set.Icc 0 (2 * π), deriv (fun θ ↦ cexp (↑θ * «I»)) θ * g (cexp (↑θ * «I»)) = Complex.I * cexp (↑θ * «I») * g (cexp (↑θ * «I»)) := by sorry
+        --refine setIntegral_congr_ae₀ ?hs ?_
+        sorry
+        }
+
+lemma winding_circle_inside (γ : closed_curve) (h_circle : ∀ t ∈ I, γ t = Complex.exp (Complex.I * 2*π* t)) (z : ℂ ) (h : norm z < 1) : ω z γ = 1 := by {
+  unfold ω
+  have h_int : ∫ (t : ℝ) in I, deriv γ.toFun t / (γ.toFun t - z) = 2*π*Complex.I := by {
+    let const : ℂ → ℂ := fun z ↦ 1
+    have integ_eq : ∫ (t : ℝ) in I, deriv γ.toFun t / (γ.toFun t - z) = ∮ (z_1 : ℂ) in C(0, 1), (z_1 - z)⁻¹ := by {
+      refine contour_integral_eq_curve_integral γ ?h_circle z fun z_1 ↦ (z_1 - z)⁻¹
+      intro t ht
+      specialize h_circle t ht
+      trivial
+    }
+    rw[integ_eq]
+    have hc1 : const z = 1 := by exact rfl
+    have haux : 2 * ↑π * Complex.I = (2 * ↑π * Complex.I ) • const z := by norm_num[hc1]
+    rw[haux]
+    have haux2 : (∮ (z_1 : ℂ) in C(0, 1), (z_1 - z)⁻¹) = (∮ (z_1 : ℂ) in C(0, 1), (z_1 - z)⁻¹ • const z_1) := by norm_num
+    rw[haux2]
+    apply DiffContOnCl.circleIntegral_sub_inv_smul (c := 0) (R := 1) (f := const) (w := z) -- can also use circleIntegral.integral_sub_inv_of_mem_ball
+    · exact diffContOnCl_const
+    · exact mem_ball_zero_iff.mpr h
+  }
+  · rw[h_int]
+    have not_zero : (2 * ↑π * Complex.I) ≠ 0:= by simp[pi_ne_zero]
+    field_simp
+}
+
+  /- Outside the unit circle we can use the fact that the function is holomorphic. For this we use the lemma
+    Complex.circleIntegral_eq_zero_of_differentiable_on_off_countable. -/
+
+  lemma winding_circle_outside (γ : closed_curve) (h_circle : ∀ t ∈ I, γ t = Complex.exp (Complex.I * 2*π* t)) (z : ℂ ) (h : norm z > 1) : ω z γ = 0 := by {
+    unfold ω
+    have h₀ : ∫ (t : ℝ) in I, deriv γ t / (γ t - z) = 0 := by {
+      let g : ℂ → ℂ := fun z_1 ↦ 1 / (z_1 - z)
+      have h_1 : ∫ (t : ℝ) in I, deriv γ t / (γ t - z) = ∮ (z_1 : ℂ) in C(0, 1), g z_1 := by {
+        exact contour_integral_eq_curve_integral γ h_circle z g}
+      rw[h_1]
+      apply Complex.circleIntegral_eq_zero_of_differentiable_on_off_countable (s := ∅)
+      · exact countable_empty
+      · have hz_1 : ∀ z_1 ∈ (closedBall 0 1), z_1 - z ≠ 0 := by {
+        intro z_1 hz_1
+        have hnorm: Complex.abs (z_1 - z) > 0 := by {
+          have rev_tri : Complex.abs (z_1 - z) ≥ |(Complex.abs z_1 - Complex.abs z)| := by exact AbsoluteValue.abs_abv_sub_le_abv_sub Complex.abs z_1 z -- reverse triangle inequality
+          have hn : (Complex.abs z_1 - Complex.abs z) ≠ 0 := by {
+            have hnaux : Complex.abs z_1 ≠  Complex.abs z := by {
+              simp at hz_1
+              simp at h
+              linarith [hz_1, h]
+            }
+            exact sub_ne_zero_of_ne hnaux
+          }
+          have hn' : norm (Complex.abs z_1 - Complex.abs z) > 0 := by exact norm_pos_iff'.mpr hn
+          exact gt_of_ge_of_gt rev_tri hn'
+        }
+        exact (AbsoluteValue.pos_iff Complex.abs).mp hnorm
+        }
+        let φ : ℂ → ℂ := fun z_1 ↦ z_1 - z
+        have haux : Continuous φ := by exact _root_.continuous_sub_right z
+        have haux' : ContinuousOn φ (closedBall 0 1) := by exact Continuous.continuousOn haux
+        apply inverse_continuous_ball
+        · exact haux'
+        · exact fun s a ↦ hz_1 s a
+      · intro z_1 hz_1
+        simp at *
+        apply inverse_differentiable_ball
+        simp
+        have hnorm: Complex.abs (z_1 - z) > 0 := by {
+          have rev_tri : Complex.abs (z_1 - z) ≥ |(Complex.abs z_1 - Complex.abs z)| := by {
+            exact AbsoluteValue.abs_abv_sub_le_abv_sub Complex.abs z_1 z
+          } -- reverse triangle inequality
+          have hn : (Complex.abs z_1 - Complex.abs z) ≠ 0 := by {
+            have hnaux : Complex.abs z_1 ≠  Complex.abs z := by {
+              linarith[hz_1, h]}
+            exact sub_ne_zero_of_ne hnaux}
+          have hn' : norm (Complex.abs z_1 - Complex.abs z) > 0 := by exact norm_pos_iff'.mpr hn
+          exact gt_of_ge_of_gt rev_tri hn'
+            }
+        · intro s hs
+          simp at hs
+          have hnorm': Complex.abs (s - z) > 0 := by {
+          have rev_tri : Complex.abs (s - z) ≥ |(Complex.abs s - Complex.abs z)| := by {
+            exact AbsoluteValue.abs_abv_sub_le_abv_sub Complex.abs s z
+          } -- reverse triangle inequality
+          have hn : (Complex.abs s - Complex.abs z) ≠ 0 := by {
+            have hnaux : Complex.abs s ≠  Complex.abs z := by {linarith[hs, h]}
+            exact sub_ne_zero_of_ne hnaux}
+          have hn' : norm (Complex.abs s - Complex.abs z) > 0 := by exact norm_pos_iff'.mpr hn
+          exact gt_of_ge_of_gt rev_tri hn'}
+          exact (AbsoluteValue.pos_iff Complex.abs).mp hnorm'
+        simp
+        linarith[hz_1]
+      · linarith
+    }
+    simp[h₀]
+}
+
 -- DISCRETE WINDING NUMBER??
 #check constant_of_derivWithin_zero
 #check Continuous.deriv_integral
+#check circleIntegral.integral_sub_inv_of_mem_ball
+#check Complex.circleIntegral_eq_zero_of_differentiable_on_off_countable
