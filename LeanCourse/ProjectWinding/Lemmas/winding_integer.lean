6aaed2fae2c660271cf798dd1de9c24aca34690e
Jorge Carrasco Coquillat <jorcarcoqui@gmail.com>
2025-01-08 19:49:39 +0100

    algún cambio más

--- LeanCourse/ProjectWinding/Lemmas/winding_integer.lean
+++ LeanCourse/ProjectWinding/Lemmas/winding_integer.lean
@@ -357,7 +357,7 @@ theorem ω_integer (γ : closed_curve) (z : ℂ) (h : ∀ t ∈ I , γ t ≠ z)
 
 -- We evaluate the values of ω when γ is the unit circle:
 
--- First we show the following useful equality:
+-- First we show the following (very) useful equality:
 
 lemma contour_integral_eq_curve_integral_15 (γ : closed_curve) (h_circle : CircleCurve_whole γ) (z : ℂ ):
 ∫ (t : ℝ) in I, deriv γ t / (γ t - z) = ∮ (z_1 : ℂ) in C(0, 1), (z_1 - z)⁻¹ := by {
@@ -552,6 +552,8 @@ lemma contour_integral_eq_curve_integral_15 (γ : closed_curve) (h_circle : Circ
     exact setIntegral_congr hmeasI fun ⦃x⦄ a ↦ h₀int x
     }
 
+-- Actually, we only need for the curve to coincide with the circle in [0,1], by our definition of Winding Number
+
 lemma contour_integral_eq_curve_integral_175 (γ : closed_curve) (h_circle : ∀ t ∈ I, γ t = Complex.exp (Complex.I * 2*π* t)) (z : ℂ ):
 ∫ (t : ℝ) in I, deriv γ t / (γ t - z) = ∮ (z_1 : ℂ) in C(0, 1), (z_1 - z)⁻¹ := by {
   have hderiv : ∀ t : ℝ, (0 < t ∧ t < 1) → HasDerivAt γ.toFun ((Complex.I * 2*π) * Complex.exp (Complex.I * 2*π* t)) t := by sorry
@@ -585,162 +587,7 @@ lemma contour_integral_eq_curve_integral_175 (γ : closed_curve) (h_circle : ∀
 }
 
 #check Set.Icc 0 (2*π)
-lemma contour_integral_eq_curve_integral_2 (γ : closed_curve) (h_circle : ∀ t ∈ I, γ t = Complex.exp (Complex.I * 2*π* t)) (z : ℂ ):
-∫ (t : ℝ) in I, deriv γ t / (γ t - z) = ∮ (z_1 : ℂ) in C(0, 1), (z_1 - z)⁻¹ := by {
-    rw[circleIntegral_def_Icc]
-    unfold circleMap
-    simp
-    have hsubs : ∫ (t : ℝ) in I, deriv γ.toFun t / (γ.toFun t - z) = ∫ (t : ℝ) in I, deriv γ.toFun t / (cexp («I» * 2 * ↑π * ↑t) - z) := by {
-      refine setIntegral_congr_ae₀ ?hs ?h -- should be easy to show
-      · exact nullMeasurableSet_Icc
-      · have haux : ∀ x ∈ I, deriv γ.toFun x / (γ.toFun x - z) = deriv γ.toFun x / (cexp («I» * 2 * ↑π * ↑x) - z) := by exact fun x a ↦ congrArg (HDiv.hDiv (deriv γ.toFun x)) (congrFun (congrArg HSub.hSub (h_circle x a)) z)
-        exact ae_of_all volume haux
-    }
-    rw[hsubs]
-    have h_deriv_1 : ∀ θ ∈ Set.Icc 0 (2*π), HasDerivAt (fun (θ : ℝ)  ↦ cexp (↑θ * «I»)) (Complex.I * cexp (↑θ * «I»)) θ := by {
-      intro θ hθ
-      have h_all : ∀ (x:ℝ) (y : ℂ), HasDerivAt (fun (x : ℝ)  ↦ cexp (x*y)) (y * cexp (x*y)) x := by {
-        intro x y
-        let h₁ : ℝ → ℂ := (fun x ↦ (x : ℂ) *y)
-        let e : ℂ → ℂ := (fun x ↦ x*y)
-        let h₂ : ℂ → ℂ := (fun z ↦ cexp z)
-        have h : HasDerivAt e y (x : ℂ) := by exact hasDerivAt_mul_const y
-        have h' : HasDerivAt h₁ y x := by exact HasDerivAt.comp_ofReal (e := e) (e' := y) (hf := h) -- By hint
-        have hh2 : HasDerivAt h₂ (cexp (h₁ x)) (h₁ x) := by exact Complex.hasDerivAt_exp (h₁ x)
-        have hder : HasDerivAt (h₂ ∘ h₁) (cexp (h₁ x) * y ) x := by exact HasDerivAt.comp x hh2 h'
-        have hxy : x * y = h₁ x := by exact rfl
-        rw[hxy]
-        have h1h2 : (fun (x : ℝ)  ↦ cexp (x * y)) = h₂ ∘ h₁ := by exact rfl
-        rw[h1h2]
-        rw[mul_comm]
-        exact hder
-      }
-      specialize h_all θ Complex.I
-      exact h_all
-    }
-    have h_deriv_1' : ∀ θ ∈ Set.Icc 0 (2*π), deriv (fun θ ↦ cexp (↑θ * «I»)) θ = Complex.I * cexp (↑θ * «I») := by {
-      intro θ hθ
-      specialize h_deriv_1 θ hθ
-      exact HasDerivAt.deriv h_deriv_1
-    }
-    have hI : I = Set.Icc 0 1 := by exact rfl
-    have h_deriv_2 : ∀ t ∈ I, HasDerivAt (fun t ↦ γ.toFun t) ((«I» * 2 * ↑π ) * cexp («I» * 2 * ↑π * ↑t)) t := by {
-      intro t ht
-      specialize h_circle t ht
-      sorry
-      }
-    have h_deriv_2': ∀ t ∈ I, deriv (fun t ↦ γ.toFun t) t = ((«I» * 2 * ↑π ) * cexp («I» * 2 * ↑π * ↑t)) := by {
-        intro t ht
-        specialize h_deriv_2 t ht
-        exact HasDerivAt.deriv h_deriv_2
-    }
-    have h_deriv_2'' : ∀ t ∈ I, deriv (fun t ↦ γ.toFun t) t / ((cexp («I» * 2 * ↑π * ↑t) - z)) = ((«I» * 2 * ↑π ) * cexp («I» * 2 * ↑π * ↑t)) / ((cexp («I» * 2 * ↑π * ↑t) - z)) := by {
-      exact fun t a ↦ congrFun (congrArg HDiv.hDiv (h_deriv_2' t a)) (cexp («I» * 2 * ↑π * ↑t) - z)
-    }
-    have h_int_eq_1 :  ∫ (θ : ℝ) in Set.Icc 0 (2 * π), deriv (fun θ ↦ cexp (↑θ * «I»)) θ * ((cexp (↑θ * «I»)) - z)⁻¹
-    = ∫ (θ : ℝ) in Set.Icc 0 (2 * π), (Complex.I * cexp (↑θ * «I»)) * ((cexp (↑θ * «I»)) - z)⁻¹ := by {
-      sorry
-    }
-    have h_int_eq_2 : ∫ (t : ℝ) in I, deriv γ.toFun t / (cexp («I» * 2 * ↑π * ↑t) - z) = ∫ (t : ℝ) in I, ((«I» * 2 * ↑π ) * cexp («I» * 2 * ↑π * ↑t)) / ((cexp («I» * 2 * ↑π * ↑t) - z)) := by {
-      sorry
-    }
-    rw[h_int_eq_1, h_int_eq_2]
-    field_simp
-    -- All left is to do a change of variables.
-    let f : ℝ → ℝ := fun x ↦ (2*π)*x
-    have hmeasI : MeasurableSet I := by exact measurableSet_Icc
-    have hf₁ : InjOn f I := by {
-      unfold InjOn
-      intro x hx y hy
-      simp_rw[f]
-      intro hexp
-      have hpos : 2 * π ≠ 0 := by {
-        simp
-        exact pi_ne_zero
-      }
-      field_simp[hpos] at hexp
-      assumption
-    }
-    let f' : ℝ → ℝ := fun x ↦ 2*π
-    have hf' : ∀ x ∈ I, HasDerivWithinAt f (f' x) I x := by {
-      have hf'' : ∀ x : ℝ, HasDerivAt f (f' x) x := by {
-        intro x
-        simp_rw[f, f']
-        -- We state the following lemma to use hasDerivAt.mul_const
-        have htrivial : (fun x ↦ 2 * π * x) =  (fun x ↦ x * (2*π )):= by {
-          ext x
-          ring
-        }
-        rw[htrivial]
-        exact hasDerivAt_mul_const (2 * π)
-      }
-      exact fun x a ↦ HasDerivAt.hasDerivWithinAt (hf'' x)
-    }
-    let g₁ : ℝ → ℂ := fun θ ↦ «I» * cexp (↑θ * «I») / (cexp (↑θ * «I») - z)
-    have hfeq : f '' I = Set.Icc 0 (2*π) := by {
-      ext y
-      constructor
-      · intro a₁
-        simp_all only [Set.mem_Icc, and_imp, implies_true, measurableSet_Icc, Set.mem_image, f, f']
-        obtain ⟨w, h⟩ := a₁
-        obtain ⟨left, right⟩ := h
-        obtain ⟨left, right_1⟩ := left
-        subst right
-        apply And.intro
-        · positivity
-        · field_simp
-          assumption
-      · intro hy₁
-        simp
-        use (y/(2*π))
-        have hpospi : (1 / 2*π) ≥ 0 := by {
-              have h' : π ≥ 0 := by exact pi_nonneg
-              have h'' : 2*π ≥ 0 := by linarith
-              field_simp[h', h'']
-              linarith
-            }
-        apply And.intro
-        · apply And.intro
-          · have hy₂ : y ≥ 0 := by {
-            simp at hy₁
-            linarith}
-            have hrepl : y / (2*π ) = y * (1/(2*π )) := by exact div_eq_mul_one_div y (2 * π)
-            rw[hrepl]
-            refine Right.mul_nonneg hy₂ ?h.left.left.hb
-            positivity
-          · simp at hy₁
-            obtain ⟨h1, h2⟩ := hy₁
-            have hrepl : y / (2*π ) = y * (1/(2*π )) := by exact div_eq_mul_one_div y (2 * π)
-            rw[hrepl]
-            calc
-            y * (1/ (2*π )) ≤ 2*π * (1/ (2*π )) := by {
-              refine mul_le_mul_of_nonneg_right h2 ?a0
-              positivity
-            }
-            _= 1 := by field_simp
-        · simp_rw[f]
-          field_simp
-    }
-    rw[← hfeq]
-    apply Eq.symm
-    have hinteq : ∫ (θ : ℝ) in f '' I, «I» * cexp (↑θ * «I») / (cexp (↑θ * «I») - z) = ∫ (θ : ℝ) in f '' I, g₁ θ := by {
-      norm_num
-      }
-    rw[hinteq]
-    simp[integral_image_eq_integral_abs_deriv_smul (f := f) (f' := f') (hf := hf₁) (hf' := hf') (g := g₁)]
-    simp_rw[f, g₁, f']
-    have hpipos : |2 * π| = 2 * π := by {
-      have hpos : 2*π ≥ 0 := by positivity
-      exact _root_.abs_of_nonneg hpos
-      }
-    rw[hpipos]
-    simp
-    have h₀int : ∀ t : ℝ, 2 * ↑π * («I» * cexp (2 * ↑π * ↑t * «I») / (cexp (2 * ↑π * ↑t * «I») - z)) = «I» * 2 * ↑π * cexp («I» * 2 * ↑π * ↑t) / (cexp («I» * 2 * ↑π * ↑t) - z) := by {
-      intro t
-      ring_nf
-    }
-    exact setIntegral_congr hmeasI fun ⦃x⦄ a ↦ h₀int x
-    }
+
 #check set_integral_congr_ae₀
 #check MeasureTheory.setIntegral_congr_ae₀
 #check integral_image_eq_integral_abs_deriv_smul
