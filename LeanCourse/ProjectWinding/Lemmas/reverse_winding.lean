c6f741543a1fe3050f3fdf574c2734f7eb6e5394
Juan Antonio Montalban Vidal <juananmv2013@gmail.com>
2025-01-23 19:31:53 +0100

    take a look at change of variables in reverse winding!!

--- LeanCourse/ProjectWinding/Lemmas/reverse_winding.lean
+++ LeanCourse/ProjectWinding/Lemmas/reverse_winding.lean
@@ -1,7 +1,8 @@
 import LeanCourse.ProjectWinding.Definitions.curves
 import Mathlib.MeasureTheory.Integral.IntervalIntegral
 import LeanCourse.ProjectWinding.Theorems.winding_integer
-
+import Mathlib.Analysis.Calculus.Deriv.Comp
+import Mathlib.MeasureTheory.Integral.SetIntegral
 
 open DifferentiableOn Finset
 open BigOperators Function Set Real Topology Filter
@@ -13,7 +14,7 @@ noncomputable section
 
 open Classical
 
-lemma ω_reverse {t : ℝ} (γ : closed_curve) (z : ℂ) (h : ∀ t : ℝ , γ t ≠ z) : ω z γ = - ω z (closed_curve_reverse γ) := by {
+lemma ω_reverse (γ : closed_curve) (z : ℂ) (h : ∀ t : ℝ , γ t ≠ z) : ω z γ = - ω z (closed_curve_reverse γ) := by {
 have h1 : ∃ n : ℤ, ω z γ = n := by {
   exact ω_integer γ z h
 }
@@ -26,32 +27,82 @@ have h2 : ω z (closed_curve_reverse γ) = -n := by {
   }
   unfold ω
   rw [h₁]
-  have h₂ : (fun t => γ (1 - t)) = (γ ∘ (λ t => 1 - t)) := by {
+  have h₂ : (fun (t : ℝ) => γ (1 - t)) = (γ ∘ (λ (t : ℝ) => ((1 - t) : ℝ))) := by {
     ext1 x
     simp
   }
-  have h₃ : deriv (fun t => γ (1 - t)) = - (deriv γ) := by {
-    ext1 x
+  have h₃ : ∀ t : ℝ,  deriv (fun (t : ℝ) => γ (1 - t)) t = - (deriv γ) (1 - t) := by {
+    intro t
     rw [h₂]
-    have h4 : DifferentiableAt ℝ γ ((fun (t : ℝ) => 1 - t) x) := by {
+    have h4 : DifferentiableAt ℝ γ ((fun (t : ℝ) => ((1 - t) : ℝ)) t) := by {
       simp
       have h5 : Differentiable ℝ γ := by exact closed_curve.Diff γ
       exact h5.differentiableAt
     }
-    have h5 : DifferentiableAt ℝ (λ t => 1 - t) x := by {
-      have h6 : DifferentiableAt ℝ id x := by {
+    have h5 : DifferentiableAt ℝ (λ (t : ℝ) => ((1 - t) : ℝ)) t := by {
+      have h6 : DifferentiableAt ℝ id t := by {
         exact differentiableAt_id
       }
-      have h7 : DifferentiableAt ℝ (fun t => (1 : ℝ)) x := by {
+      have h7 : DifferentiableAt ℝ (fun (_ : ℝ) => (1 : ℝ)) t := by {
         exact differentiableAt_const 1
       }
       exact DifferentiableAt.sub h7 h6
     }
-    sorry
+    have h6 : deriv (γ ∘ fun (t : ℝ) ↦ ((1 - t) : ℝ)) t = deriv γ ((fun (t : ℝ) => 1 - t) t) * deriv (fun (t : ℝ) => ((1 - t) : ℝ)) t := by {
+      have h9 : NormedSpace ℝ ℝ := by infer_instance
+      --exact deriv.comp h4 h5
+      sorry
+    }
+    rw [h6]
+    have h7 : deriv (fun (t : ℝ) => ((1 - t) : ℝ)) t = -1 := by {
+      let f : ℝ → ℝ := fun t => 1
+      have h8 : deriv f t = 0 := by {
+        sorry
+      }
+      have h9 : deriv (λ (t : ℝ) => t) t = 1 := by {
+        sorry
+      }
+      --exact deriv_sub h8 h9
+      sorry
+    }
+    rw [h7]
+    simp
   }
-  rw [h₃]
+  simp only [h₃]
+  have heq : ∀ t : ℝ, deriv (fun t ↦ γ.toFun (1 - t)) t / (γ.toFun (1 - t) - z) =
+    -deriv γ (1 - t) / (γ (1 - t) - z) := by {
+    intro t
+    rw [h₃]
+  }
+  have hs : MeasurableSet I := by exact measurableSet_Icc
+  have haux : EqOn (fun t => deriv (fun t ↦ γ.toFun (1 - t)) t / (γ.toFun (1 - t) - z)) (fun t => - deriv γ (1 - t) / (γ (1 - t) - z)) I := by {
+    intros x hx
+    simp
+    rw [heq]
+  }
+  have haux1 : ∀ t : ℝ, -deriv γ.toFun (1 - t) / (γ.toFun (1 - t) - z) =
+  - (deriv γ.toFun (1 - t) / (γ.toFun (1 - t) - z)) := by {
+    intro t
+    ring
+  }
+  have neg : 1 / (2 * ↑π * Complex.I) * ∫ (t : ℝ) in I, - (deriv γ.toFun (1 - t) / (γ.toFun (1 - t) - z)) =
+    - (1 / (2 * ↑π * Complex.I) * ∫ (t : ℝ) in I, deriv γ.toFun (1 - t) / (γ.toFun (1 - t) - z)) := by {
+    rw [integral_neg]
+    simp
+  }
+  simp only [haux1]
+  rw [neg]
+  have hint : ∫ (t : ℝ) in I, deriv γ.toFun (1 - t) / (γ.toFun (1 - t) - z) =
+    ∫ (x : ℝ) in (1)..(0), deriv γ x / (γ x - z) := by {
+      sorry
+  }
+  rw [hint]
+  have hint1 : ∫ (x : ℝ) in (1)..(0), deriv γ x / (γ x - z) = - ∫ (x : ℝ) in (0)..(1), deriv γ x / (γ x - z) := by {
+    exact intervalIntegral.integral_symm 0 1
+  }
+  rw [hint1]
   sorry
 }
 rw [h2]
-sorry
+ring
 }
